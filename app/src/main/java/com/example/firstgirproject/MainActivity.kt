41938c2467a50052031ce518f5353b337d25f708
dev_mohsenJafari <dev.mohsenjafaritadi@gmail.com>
2023-11-11 11:48:57 +0330

    some toast files added

--- app/src/main/java/com/example/firstgirproject/MainActivity.kt
+++ app/src/main/java/com/example/firstgirproject/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.firstgirproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"hello to all after first commit",Toast.LENGTH_LONG).show()
+        Toast.makeText(this,"toast number1",Toast.LENGTH_LONG).show()
+        Toast.makeText(this,"toast number2",Toast.LENGTH_LONG).show()
+        Toast.makeText(this,"toast number3",Toast.LENGTH_LONG).show()
     }
 }
